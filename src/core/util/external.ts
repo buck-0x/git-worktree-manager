8460a1bc596000ca0d31b68e72dae2e55824a006
jackiotyu <2504448153@qq.com>
2025-07-03 09:15:06 +0800

    feat(config): change default value of openInsideFolder and update dependencies
    - Set default value of openInsideFolder to true
    - Update @rspack/cli and @rspack/core to version 1.4.2
    - Update @swc/helpers to version 0.5.17
    - Remove @types/react from devDependencies

--- src/core/util/external.ts
+++ src/core/util/external.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
-import { open }  from '@/core/util/open';
 
 export const openExternalTerminal = (path: string) => {
     return vscode.commands.executeCommand('openInTerminal', vscode.Uri.file(path));
 };
 
 export const revealFolderInOS = (folder: string) => {
-    return open(folder);
+    return vscode.env.openExternal(vscode.Uri.file(folder));
 };
